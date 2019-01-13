071facecdc67decb53ab6e5e13a0c7592ae962a1
khensura.loveel Love EL <khensura.loveel@gmail.com>
2019-01-13 07:13:03 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+function paragraphSelector () {
+   return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+
+function firstListItem () {
+  
+  $('ul li:first-child')
+}
